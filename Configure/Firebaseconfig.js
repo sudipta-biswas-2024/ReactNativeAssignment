84e7ef9a90017d4b05d0ad8ab4e60cc175dedf6c
Sudipta Biswas <sudipta.biswas1988@gmail.com>
2024-09-09 19:22:07 +0530

    updated

--- Configure/Firebaseconfig.js
+++ Configure/Firebaseconfig.js
@@ -4,12 +4,12 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAZELEPEsalPJ3F-7HmaE62MQhe33GY4zo",
+    apiKey: "Your API Key goes here",
     authDomain: "loginappreactnative-db6ed.firebaseapp.com",
     projectId: "loginappreactnative-db6ed",
     storageBucket: "loginappreactnative-db6ed.appspot.com",
     messagingSenderId: "920742512484",
-    appId: "1:920742512484:web:1b194bfa85169906779c71"
+    appId: "Your APP ID goes here"
 };
 
 // Initialize Firebase
